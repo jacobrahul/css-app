2406617eae8fae8d0e69e09fc7034eea238d7be5
jacobrahul <jacobrahulb@gmail.com>
2025-11-02 02:03:40 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,7 @@
-// Reserved for gating /app and /admin later
+export function middleware() {
+// temporarily do nothing; let all requests pass
+}
+
+export const config = {
+matcher: ["/(.*)"]
+};// Reserved for gating /app and /admin later
